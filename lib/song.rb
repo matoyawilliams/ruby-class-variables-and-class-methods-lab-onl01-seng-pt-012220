a4d1c4d26128de0d69d3611b99dc9dc6efd8fa36
Matoya Williams <matoyawilliams@yahoo.com>
2020-02-17 22:41:08 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -18,8 +18,7 @@ lass Song
   def self.genres
     @@genres.uniq
   end
-  
-  # this method is the one I found on stackexchange...
+ 
   def self.genre_count
     @@genres.inject(Hash.new(0)) { |total, i| total[i] += 1 ;total}
   end 
